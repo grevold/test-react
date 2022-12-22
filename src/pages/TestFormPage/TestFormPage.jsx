c31675ab60aeba0e4b1a88ed759dee2ad8f4dca5
Alexey Grevtsov <grevold@yandex.ru>
2022-12-22 16:57:22 +0300

    Добавил покрас текста в различные цвета в зависимости от длины текста

--- src/pages/TestFormPage/TestFormPage.jsx
+++ src/pages/TestFormPage/TestFormPage.jsx
@@ -16,13 +16,32 @@ export function TestFormPage() {
     const input = refInput.current;
     if (!input) return;
     input.value = "";
+    input.style = 'color: black';
   };
+
+  function handleInputChange(event) {
+    const input = refInput.current;
+    if (!input) return;
+    const text = event.target.value;
+    // Если текст короткий
+    if(text.length < 4) {
+      input.style = 'color: red';
+    }
+    // Если текст средней длины
+    if(text.length >= 5 && text.length < 8) {
+      input.style = 'color: yellow';
+    }
+    // Если текст длинный
+    if(text.length >= 8) {
+      input.style = 'color: green';
+    }
+  }
 
   return (
     <div>
       <Header />
       <div className={s.container}>
-        <input ref={refInput} className={s.input} />
+        <input onChange={handleInputChange} ref={refInput} className={s.input} />
         <button onClick={alertMessage} className={s.button}>
           Вывести сообщение
         </button>
